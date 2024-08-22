78bf1d911fbf9d4468b05c911a70f22fe4d35b14
Nicolas marcaud <nmarcau@gmail.com>
2024-08-22 21:35:01 +0200

    feat: routing and route parameters

--- pages/course.vue
+++ pages/course.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
-  >
+  <div class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center">
     <div class="prose mb-12">
       <h1>
         <span class="font-medium">
@@ -12,16 +10,12 @@
     </div>
 
     <div class="flex flex-row justify-center flex-grow">
-      <div
-        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
-      >
+      <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col">
         <h3>Chapters</h3>
         <!-- All the lessons for the course listed here -->
       </div>
-
       <div class="prose p-12 bg-red-100 rounded-md w-[65ch]">
-        <h2>Lesson</h2>
-        <p>This is a lesson</p>
+        <NuxtPage />
       </div>
     </div>
   </div>
---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>{{ $route.params.lessonSlug }}</h2>
+  <p>This is a lesson</p>
+</template>
